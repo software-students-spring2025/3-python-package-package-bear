b7c0838dc336de4d646cd7587dbf4a719f014c9e
jasmine7310 <jf4654@nyu.edu>
2025-03-11 14:48:30 -0400

    Finish Inital Setup

---
+++ src/buildabearpackage/__main__.py
@@ -0,0 +1,11 @@
+import buildabearpackage.buildabear as buildabear
+
+def main():
+    pass
+    # ^^ pass is to keep compiler happy 
+    # (or else error saying expected indent)
+    # DELETE PASS WHEN WORKING ON CODE!!!!
+    #
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_buildabear.py
@@ -0,0 +1,2 @@
+import pytest
+from buildabearpackage import buildabear
