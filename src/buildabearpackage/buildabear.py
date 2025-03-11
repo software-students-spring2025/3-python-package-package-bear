7fda581ce439355f3a32c299ea736806b6024a1b
jasmine7310 <jf4654@nyu.edu>
2025-03-11 15:28:25 -0400

    Function setup

--- src/buildabearpackage/buildabear.py
+++ src/buildabearpackage/buildabear.py
@@ -1,1 +1,26 @@
 # Our functions go here
+
+happiness = 0
+hunger = 0
+cleanliness = 0
+
+def play():
+    pass
+
+def check_status():
+    pass
+
+def update_status():
+    pass
+
+def change_name():
+    pass
+
+def update_happiness():
+    pass 
+
+def update_cleanliness():
+    pass
+
+def update_hunger():
+    pass
