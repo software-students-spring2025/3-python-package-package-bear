4c5ae3d5f5756ef270f2caf6e0ebc1a785b17b2f
jasmine7310 <jf4654@nyu.edu>
2025-03-13 14:57:41 -0400

    Added directions for functions for team clarity, fixed README

---
+++ src/buildabearpackage/__init__.py
@@ -0,0 +1,2 @@
+# Do not delete file
+# Need this file to for Python to recognize the directory as a "regular" package
--- src/buildabearpackage/buildabear.py
+++ src/buildabearpackage/buildabear.py
@@ -1,26 +1,97 @@
-# Our functions go here
-
 happiness = 0
 hunger = 0
 cleanliness = 0
+budget = 100
+name = "Bear"
+initialized = False
 
+# need variables for timer/ clock
+
+# - We might need to create a quit function 
+#   that saves the data to a local DB to save progress
+# - Restart/ die funtion
+# ^^^^ OPTIONAL IMPLEMENTATION
+
+# ******** Alex
+# This function sets up the inital pet
+# and starts the clock/ timing for
+# hunger, cleanliness, and happiness to go down
+# Also show initial startup diaglogue & instructions
+# Something like : Hi! I'm Bear! Nice to meet you!
+# Feed me to keep me happy! Work a job to earn money!... etc
+# MOST IMPORTANT DIRECTION:
+# TELL USER TO USE check_status after using play to check on pet
+# play is only the initializer!!!!
+# !!!! SET INITIALIZED TO TRUE AFTER!!!
+# !!!!! MIGHT NEED TO CHANGE THIS LATER IN MAIN WHEN RUN/ PACKAGE AND NOT AS A FUNCTION!!!!!
 def play():
     pass
 
+# ******** Jasmine
+# This function prints out the current status of the pet
+# along with the correct emoticons
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def check_status():
     pass
 
+# ******** Jasmine
+# This function uses the clock/ timer to update the 
+# hunger, happiness, and cleanliness over time
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def update_status():
     pass
 
+# ******** Sophia
+# This function allows the user to change the name of the pet
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def change_name():
     pass
 
+# ******** Alex
+# This function uses the timer/ clock to periodically update the happiness
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def update_happiness():
     pass 
 
+# ******** Tadelin
+# This function uses the timer/ clock to periodically update the cleanliness
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def update_cleanliness():
     pass
 
+# ******** Tadelin
+# This function uses the timer/ clock to periodically update the hunger
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def update_hunger():
     pass
+
+# ******** Sophia
+# This function takes a certain int of hours to work
+# $16/ hour (or how many you want)
+# Updates budget!!
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
+def work(hours: int):
+    pass
+
+# ******** Sophia
+# This function allows teh user to buy food for a set int amount
+# THIS FUNCTION CAN ONLY RUN IF PLAY() IS CALLED
+# (INITIALIZED == TRUE)
+# IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
+def buy_food(amount: int):
+    pass
+
