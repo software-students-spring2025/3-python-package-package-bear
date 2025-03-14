74997f5528d748f5fc1369c337a4598940d17217
TadelinD <td2349@nyu.edu>
2025-03-14 17:05:12 -0400

    update Hunger and update Cleanliness

--- src/buildabearpackage/buildabear.py
+++ src/buildabearpackage/buildabear.py
@@ -5,6 +5,7 @@ cleanliness = 0
 budget = 100
 name = "Bear"
 initialized = False
+lastChecked = 0
 
 # need variables for timer/ clock
 
@@ -27,11 +28,12 @@ initialized = False
 # !!!! SET INITIALIZED TO TRUE AFTER!!!
 # !!!!! MIGHT NEED TO CHANGE THIS LATER IN MAIN WHEN RUN/ PACKAGE AND NOT AS A FUNCTION!!!!!
 def play():
-    global happiness, hunger, cleanliness, initialized
+    global happiness, hunger, cleanliness, initialized, lastChecked
     initialized = True
     happiness = 100
     hunger = 100
     cleanliness = 100
+    lastChecked = time.time()
     print("Hi! I'm Bear! Nice to meet you!")
     print("Feed me to keep me happy! Work a job to earn money!")
     print("Use check_status to check my current happiness, hunger, and cleanliness!")
@@ -40,6 +42,7 @@ def play():
         update_happiness()
         update_cleanliness()
         update_hunger()
+        lastChecked = time.time()
 
 
 
@@ -89,7 +92,13 @@ def update_happiness():
 # (INITIALIZED == TRUE)
 # IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def update_cleanliness():
-    pass
+    global cleanliness
+    if not initialized:
+        print("Make sure to use play() to set up the bear!")
+        return
+    currentTime = time.time()
+    timePassed = int(lastChecked - currentTime)
+    cleanliness -= (timePassed/100)
 
 # ******** Tadelin
 # This function uses the timer/ clock to periodically update the hunger
@@ -97,7 +106,13 @@ def update_cleanliness():
 # (INITIALIZED == TRUE)
 # IF NOT INIT, PRINT DIRECTION TO CALL PLAY()
 def update_hunger():
-    pass
+    global hunger
+    if not initialized:
+        print("Make sure to use play() to set up the bear!")
+        return
+    currentTime = time.time()
+    timePassed = int(lastChecked - currentTime)
+    hunger -= (timePassed/100)
 
 # ******** Sophia
 # This function takes a certain int of hours to work
