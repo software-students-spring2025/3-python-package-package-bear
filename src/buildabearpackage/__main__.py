d5c3b23c2aeec3ff4310e08a153b67aee7653dd9
TadelinD <td2349@nyu.edu>
2025-03-18 16:43:02 -0400

    Added to main function

--- src/buildabearpackage/__main__.py
+++ src/buildabearpackage/__main__.py
@@ -1,12 +1,31 @@
-from src.buildabearpackage import buildabear
+import buildabear as buildabear
+import sys
 
 def main():
-    pass
     # ^^ pass is to keep compiler happy 
     # (or else error saying expected indent)
     # DELETE PASS WHEN WORKING ON CODE!!!!
-    print("Welcome to Package Bear! Your very own command line pet!")
-    print("Run the command play() to get started!")
+    if "start" in sys.argv[1:]:
+        buildabear.play()
+    elif "check status" in sys.argv[1:]:
+        buildabear.check_status()
+    elif "change name" in sys.argv[1:]:
+        buildabear.change_name()
+    elif "work" in sys.argv[1:]:
+        buildabear.work(sys.argv[2])
+    elif "feed" in sys.argv[1:]:
+        buildabear.feed_bear()
+    elif "clean" in sys.argv[1:]:
+        buildabear.clean_bear()
+    elif "play" in sys.argv[1:]:
+        buildabear.play_with_bear()
+    elif "buy food" in sys.argv[1:]:
+        buildabear.buy_food(sys.argv[3])
+    else:
+        print("Welcome to Package Bear! Your very own command line pet!")
+        print("Try using 'start' to create your bear!")
+    
+
 
 if __name__ == "__main__":
     main()
