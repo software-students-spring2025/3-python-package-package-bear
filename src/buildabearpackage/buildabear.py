b8de5c072ec91aa17f894fcfa5e578837b1ec99d
TadelinD <td2349@nyu.edu>
2025-03-19 23:43:28 -0400

    reverting changes

--- src/buildabearpackage/buildabear.py
+++ src/buildabearpackage/buildabear.py
@@ -1,35 +1,14 @@
 import time
 import random
 
-data = open('Data.txt', 'r') 
-happyLine = data.readline().split(":")
-happiness = int(happyLine[1])
-
-hungerLine = data.readline().split(":")
-hunger = int(hungerLine[1])
-
-cleanLine = data.readline().split(":")
-cleanliness = int(cleanLine[1])
-
-foodLine = data.readline().split(":")
-food = int(foodLine[1])
-
-budgetLine = data.readline().split(":")
-budget = int(budgetLine[1])
-
-nameLine = data.readline().split(":")
-name = nameLine[1]
-
-initializedLine = data.readline().split(":")
-if initializedLine[1] == "False":
-    initialized = False
-else:
-    initialized = True
-
-lastCheckedLine = data.readline().split(":")
-lastChecked = int(lastCheckedLine[1])
-
-data.close()
+happiness = 0
+hunger = 0
+cleanliness = 0
+food = 0
+budget = 0
+name = "Bear"
+initialized = False
+lastChecked = 0
 
 # need variables for timer/ clock
 
